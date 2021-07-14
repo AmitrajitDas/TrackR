82185283d857a21b87d05a647e968e6f5010d7a0
Amitrajit Das <amitrajitdas31@gmail.com>
2021-07-14 21:33:57 +0530

    linting and mui templating

--- src/App.js
+++ src/App.js
@@ -1,5 +1,42 @@
-function App() {
-  return <div className='App'>Hi</div>
+import {
+  MuiThemeProvider,
+  createMuiTheme,
+  responsiveFontSizes,
+  Container,
+  Paper,
+} from '@material-ui/core'
+
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+
+const App = () => {
+  const theme = responsiveFontSizes(
+    createMuiTheme({
+      typography: {
+        fontFamily: '"Ubuntu"',
+        color: {
+          main: '#fff',
+          secondary: 'rgba(255, 255, 255, 0.7)',
+          disabled: 'rgba(255, 255, 255, 0.5)',
+        },
+      },
+      palette: {
+        type: 'dark',
+        primary: { main: '#334257', secondary: '#66DE93' },
+        secondary: { main: '#ffffff' },
+        alternate: { main: '#161616' },
+      },
+    })
+  )
+
+  return (
+    <>
+      <MuiThemeProvider theme={theme}>
+        <Paper>
+          <Router>Hi</Router>
+        </Paper>
+      </MuiThemeProvider>
+    </>
+  )
 }
 
 export default App
