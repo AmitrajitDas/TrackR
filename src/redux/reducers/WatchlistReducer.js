c8408598ee5568c832586f13c6bf233f3690c653
Amitrajit Das <amitrajitdas31@gmail.com>
2021-07-20 22:26:57 +0530

    removeFromWatchList reducer

--- src/redux/reducers/WatchlistReducer.js
+++ src/redux/reducers/WatchlistReducer.js
@@ -1,9 +1,19 @@
-import { ADD_TO_WATCHLIST } from '../constants/WatchConstants'
+import {
+  ADD_TO_WATCHLIST,
+  REMOVE_FROM_WATCHLIST,
+} from '../constants/WatchConstants'
 
 export const watchlistReducer = (state = { watchlistMovies: [] }, action) => {
   switch (action.type) {
     case ADD_TO_WATCHLIST:
       return { watchlistMovies: [action.payload, ...state.watchlistMovies] }
+    case REMOVE_FROM_WATCHLIST:
+      return {
+        ...state,
+        watchlistMovies: state.watchlistMovies.filter(
+          (movie) => movie.id !== action.payload
+        ),
+      }
     default:
       return state
   }
