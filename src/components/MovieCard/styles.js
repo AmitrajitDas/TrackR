67c8acb960162447950761998c3e236381833286
AmitrajitDas <amitrajitdas31@gmail.com>
2021-08-01 21:56:02 +0530

    bug fixed

--- src/components/MovieCard/styles.js
+++ src/components/MovieCard/styles.js
@@ -2,7 +2,6 @@ import { makeStyles } from '@material-ui/core/styles'
 
 export const useStyles = makeStyles((theme) => ({
   wrapper: {
-    backgroundColor: theme.palette.body.main,
     padding: '5rem',
   },
 
